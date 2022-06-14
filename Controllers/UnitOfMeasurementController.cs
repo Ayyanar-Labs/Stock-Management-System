fc99cb63afb389cf3bbec1f03ec3ec4209cbf145
Ayyanar <48320634+ayyanar-snkl@users.noreply.github.com>
2022-06-14 20:39:34 +0530

    Update-3
    Update-3

---
+++ Controllers/UnitOfMeasurementController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockManagementSystem.Controllers
+{
+    public class UnitOfMeasurementController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+    }
+}
